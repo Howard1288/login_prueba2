615105d0607d59e023401a7cc833e09173315e72
¨Howard <programadorhaaj.2023@gmail.com>
2023-08-16 09:53:51 -0600

    Actualizando el login

---
+++ views/login/index.php
@@ -0,0 +1,23 @@
+<h2 class="text-center mb-4 text-primary">lnicio de sesión</h2>
+<div class="justify-content-center">
+    <form class="col-lg-4 border border rounded p-3">
+        <div class="row mb-3">
+            <div class="col">
+                <label for="usu_catalogo" class="form-label">Catalogo</label><br />
+                <input type="number" class="form-control" id="usu-catalogo">
+            </div>
+        </div>
+        <div class="row mb-3">
+            <label for="usu_password" class="form-label">Contraseña</label><br />
+            <input type="password" class="form-control" id="usu_password">
+        </div>
+</div>
+<div class="d-grid">
+    <button class="btn btn-primary" type="submit"> Iniciar Sesion</button>
+
+</div>
+</form>
+<div class="mt-3">
+    <p class="mb-0 text-center"> No Tiene Una Cuenta? <a href="/login_prueba/registro" class="text-primary fw-bold ms-2">Registrese</a></p>
+</div>
+</div>
