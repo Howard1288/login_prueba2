34d8dea29d508876cec5df3aa48965327d2465c7
¨Howard <programadorhaaj.2023@gmail.com>
2023-08-16 11:49:33 -0600

    Actualizado completado

--- views/login/index.php
+++ views/login/index.php
@@ -1,24 +1,24 @@
-<h2 class="text-center mb-4 text-primary">lnicio de sesión</h2>
-<div class="d-flex justify-content-center align-items-center vh-50">
-    <form class="col-lg-4 border border rounded p-3">
-        <div class="row mb-3">
-            <div class="col">
-                <label for="usu_catalogo" class="form-label">Catalogo</label><br />
-                <input type="number" class="form-control" id="usu-catalogo">
+<h2 class="text-center mb-4 text-primary">Inicio de Sesión</h2>
+<div class="row justify-content-center" >
+        <form class="col-lg-4 border rounded p-3">
+            <div class="row mb-3">
+                <div class="col">
+                    <label for="usu_catalogo" class="form-label">Catálogo</label>
+                    <input type="number" class="form-control" id="usu_catalogo" name="usu_catalogo">
+                </div>
+                <div class="row mb-3">
+                    <div class="col">
+                        <label for="usu_password" class="form-label">Contraseña</label>
+                        <input type="password" class="form-control" id="usu_password" name="usu_password">
+                    </div>
+                </div>
+                <div class="d-grid">
+                    <button type="submit" class="btn btn-primary">Iniciar Sesión</button>
+                </div>
             </div>
-        </div>
-        <div class="row mb-3">
-            <label for="usu_password" class="form-label">Contraseña</label><br />
-            <input type="password" class="form-control" id="usu_password">
-        </div>
-</div>
-<div class="row mb-3">
-    <button class="btn btn-primary" type="submit">Iniciar Sesion</button>
-
-</div>
-</form>
-<div class="mt-3">
-    <p class="mb-0 text-center"> No Tiene Una Cuenta? <a href="/login_prueba/registro" class="text-primary fw-bold ms-2">Registrese</a></p>
-</div>
-<script src="<?= asset('.build/js/login/index.js') ?>"></script>
+        </form>
+    </div class="mt-3">
+    <p class="mb-0 text-center">¿No tiene una cuenta?<a href="/login/registro" class="text-primary fw-bold ms-2">Registrarse</a></p> 
+    <script src="<?= asset('./build/js/login/index.js') ?>"></script>
+    </div>
 </div>
